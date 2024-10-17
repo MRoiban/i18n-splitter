68c86f2fece1e8f829592fb9c270e68118fb7acb
MRoiban <roiban.marius.alexandru@gmail.com>
2024-10-17 10:50:52 +0200

    Add source and target language options to split_i18n_file function

--- split.py
+++ split.py
@@ -1,11 +1,11 @@
 import os
 import argparse
 
-def split_i18n_file(input_file, lines_per_file=50):
+def split_i18n_file(input_file, source_language, target_language, lines_per_file=50):
     with open(input_file, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
-    original_filename, _ = os.path.splitext(input_file)
+    original_filename, _ = os.path.splitext(os.path.basename(input_file))
     total_pieces = len(lines) // lines_per_file + (1 if len(lines) % lines_per_file else 0)
 
     # Create the 'build' folder if it doesn't exist
@@ -15,7 +15,7 @@ def split_i18n_file(input_file, lines_per_file=50):
     for piece_number in range(total_pieces):
         start_line = piece_number * lines_per_file
         end_line = start_line + lines_per_file
-        piece_content = lines[start_line:end_line] # ["translate the entire i18n file i'm giving you, make sure you translate it all in french:\n"] + 
+        piece_content = [f"translate the entire i18n file i'm giving you, make sure you translate it all from {source_language} to {target_language}:"] + lines[start_line:end_line]
 
         piece_filename = f"./build/{original_filename}-{piece_number + 1}.txt"
         with open(piece_filename, 'w', encoding='utf-8') as piece_file:
@@ -26,6 +26,8 @@ def split_i18n_file(input_file, lines_per_file=50):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split i18n file into pieces.')
     parser.add_argument('input_file', help='Path to the input file')
+    parser.add_argument('--source_language', '-s', required=True, help='Source language of the file')
+    parser.add_argument('--target_language', '-t', required=True, help='Target language for translation')
     args = parser.parse_args()
 
-    split_i18n_file(args.input_file, lines_per_file=50)
+    split_i18n_file(args.input_file, args.source_language, args.target_language, lines_per_file=50)
